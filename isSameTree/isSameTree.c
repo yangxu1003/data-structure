86d494d5778cc6d36f2fbd8cf07f494d26d1372f
yangxu1003 <1773349271@qq.com>
2021-01-19 19:05:19 +0800

    判断二个二叉树是否相同

---
+++ isSameTree/isSameTree.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+    //比较当前结点
+    //结构比较
+    if (p == NULL && q != NULL)
+    {
+        return false;
+    }
+    if (p != NULL && q == NULL)
+    {
+        return false;
+    }
+
+    //值比较
+    if (p->val != q->val)
+    {
+        return false;
+    }
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
